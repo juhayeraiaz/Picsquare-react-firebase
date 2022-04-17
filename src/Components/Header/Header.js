a031dbdb40e40dd85c1c71427d9325aa08f0702f
juhayerayaz <aiazj97@gmail.com>
2022-04-17 13:21:49 +0600

    initializing project and setting up header and footer

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import logo from '../../images/site-logo.png'
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" sticky="top">
+                <Container>
+                    <Navbar.Brand href="#home"><img
+                        src={logo}
+                        height="40"
+                        className="d-inline-block align-top"
+                        alt="React Bootstrap logo"
+                    /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
