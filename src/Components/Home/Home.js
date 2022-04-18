7c64719cbeab1c70cb0152654a4342af314f848a
juhayerayaz <aiazj97@gmail.com>
2022-04-18 18:52:40 +0600

    project complete

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner'
 import Services from '../Services/Services';
+import Works from '../Works/Works';
 
 const Home = () => {
     return (
         <>
             <Banner></Banner>
             <Services></Services>
+            <Works></Works>
         </>
     );
 };
